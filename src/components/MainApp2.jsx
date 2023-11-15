2ac0eb8921b47396d2e2dbf9ad1e78ea7bff37cb
anaslopezl <ana_Sofia.lopez@uao.edu.co>
2023-11-14 22:06:03 -0500

    16

---
+++ src/components/MainApp2.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment } from '../Store/slices/CounterSlice'
+import { decrement } from '../Store/slices/CounterSlice'
+
+export const MainApp2 = () => {
+
+  const { counter } = useSelector ( state => state.counter);
+  const  dispatch = useDispatch();
+
+  return (
+    <>
+        <h1>MainApp</h1>
+        <hr/>
+        <span> Counter is: {counter}</span><br/>
+        <button
+          className='btn btn-primary'
+          onClick={ () => dispatch( increment() )}
+        > Actualizar</button><br/>
+        <button
+          className='btn btn-primary'
+          onClick={ () => dispatch (decrement())}
+          > Eliminar</button>
+    </>
+  )
+}
